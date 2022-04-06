68f2544ea55e1efe32b3dd8a495b475717d50cfc
ellaaaaa <102322770+3llaaa@users.noreply.github.com>
2022-04-06 19:25:47 +0000

    nothing

---
+++ clothes.py
@@ -0,0 +1,15 @@
+class Shirt:
+  
+  def __init__(self,color,numButtons,isShortSleeve,hasCollar):
+    self.color = color
+    self.numButtons = numButtons
+    self.isShortSleeve = isShortSleeve
+    self.hasCollar = hasCollar
+
+
+class Pants:
+
+  def __init__(self,color,waistSize,length):
+    self.color = color
+    self.waistSize = waistSize
+    self.length = length
