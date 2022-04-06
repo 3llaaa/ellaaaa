f9f22f4d3883089bcdc5fd017b7d026c4cdd6127
ellaaaaa <102322770+3llaaa@users.noreply.github.com>
2022-04-06 18:51:49 +0000

    nothing

---
+++ dogFile.py
@@ -0,0 +1,8 @@
+class dog():
+  def __init__(self, gender, color, fur):
+    self.gender = gender
+    self.color = color
+    self.fur = fur
+
+Elladog = dog("female", "dark brown", "brown")
+print(Elladog.fur + Elladog. spped)
